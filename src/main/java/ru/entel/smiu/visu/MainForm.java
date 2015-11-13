49b65112b77e676b7037dd751b726e9316d52892
farades <creg2007@gmail.com>
2015-11-13 17:53:31 +0300

    test commit from idea

--- src/main/java/ru/entel/smiu/visu/MainForm.java
+++ src/main/java/ru/entel/smiu/visu/MainForm.java
@@ -1,27 +1,41 @@
 package ru.entel.smiu.visu;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainForm extends JFrame {
     private JButton button;
     private JPanel rootPanel;
+    private JButton normalButton;
+    private JTabbedPane tabbedPane1;
+    private JPanel tab1;
+    private JPanel tab2;
+
+    private GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
 
     public MainForm() {
         super("СМИУ ENTEL");
         setContentPane(rootPanel);
-
-        pack();
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
 
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showConfirmDialog(MainForm.this, "You clicked");
+                device.setFullScreenWindow(MainForm.this);
             }
         });
 
+        normalButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                device.setFullScreenWindow(null);
+            }
+        });
+
+        pack();
         setVisible(true);
     }
 }
